6b78bccf35df7f3099948dde1f630ed3b4cbb9c5
UmbertoDV <biccoumby2002@hotmail.com>
2023-01-27 17:42:58 +0100

    indents

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -1,19 +1,19 @@
 // Dato un array contenente i risultati di una squadra (vittorie, pareggi e sconfitte) [W, W, L, X, X, W, L, X]
 // Stampare a video il numero di vittorie, il numero di pareggi ed il numero di sconfitte.
 const risultati = [
-  "W",
-  "W",
-  "L",
-  "X",
-  "X",
-  "W",
-  "L",
-  "X",
-  "L",
-  "L",
-  "L",
-  "L",
-  "L",
+	"W",
+	"W",
+	"L",
+	"X",
+	"X",
+	"W",
+	"L",
+	"X",
+	"L",
+	"L",
+	"L",
+	"L",
+	"L",
 ];
 
 let sconfitte = 0;
@@ -21,13 +21,13 @@ let vittorie = 0;
 let pareggi = 0;
 
 for (let i = 0; i < risultati.length; i++) {
-  if (risultati[i] == "W") {
-    vittorie++;
-  } else if (risultati[i] == "X") {
-    pareggi++;
-  } else if (risultati[i] == "L") {
-    sconfitte++;
-  }
+	if (risultati[i] == "W") {
+		vittorie++;
+	} else if (risultati[i] == "X") {
+		pareggi++;
+	} else if (risultati[i] == "L") {
+		sconfitte++;
+	}
 }
 
 console.log(`sconfitte: ${sconfitte}`);
--- JSnack/js/main.js
+++ JSnack/js/main.js
@@ -2,8 +2,8 @@
 const allNumbers = [];
 
 for (let i = 0; i < 10; i++) {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  allNumbers.push(randomNumber);
+	const randomNumber = Math.floor(Math.random() * 100) + 1;
+	allNumbers.push(randomNumber);
 }
 console.log(allNumbers);
 
@@ -11,10 +11,10 @@ console.log(allNumbers);
 let totNumeriDispari = 0;
 
 for (let i = 0; i < allNumbers.length; i++) {
-  if (allNumbers[i] % 2 !== 0) {
-    console.log(`Numero dispari: ${allNumbers[i]}`);
-    totNumeriDispari = totNumeriDispari + allNumbers[i];
-  }
+	if (allNumbers[i] % 2 !== 0) {
+		console.log(`Numero dispari: ${allNumbers[i]}`);
+		totNumeriDispari = totNumeriDispari + allNumbers[i];
+	}
 }
 
 console.log(totNumeriDispari);
